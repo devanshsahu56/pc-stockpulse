c661e73730391137bc2b751b40f7914629fb7dd3
devanshsahu56 <devanshsahu700@gmail.com>
2026-07-17 19:49:13 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'https://wholesalehub-backend-mqby.onrender.com',
+    'https://pcwholescalehub.space/'
     /\.vercel\.app$/
   ],
   credentials: true
